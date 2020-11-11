bf61ff1d500f58e1c500cde3a42aa3b71572c481
Eric <eric.m.lombardo@gmail.com>
2020-11-11 12:57:29 -0500

    Reverting to original code after testing git commmits

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,6 +1,3 @@
-# testing 
-# testing
-#testing
 def unsafe?(speed)
   if speed < 40 || speed > 60
     true
@@ -15,7 +12,6 @@ def not_safe?(speed)
 	speed < 40 || speed > 60 ? true : false
 end
 
-# testing
 	
 
 
